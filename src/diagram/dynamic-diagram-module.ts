844e5214207d28ff5d879196ffd426fff33ae0ed
Giuliano Marinelli <giuliano.marinelli@hotmail.com>
2025-02-18 14:29:52 -0300

    updated diagram container, now it receives diagramOptions, viewerOptions and externalServices, and initialize them internally. now diagramOptions can be used in diagram module initialization. projection view is now only available in editable mode.

--- src/diagram/dynamic-diagram-module.ts
+++ src/diagram/dynamic-diagram-module.ts
@@ -3,105 +3,128 @@ import {
   ContainerConfiguration,
   DEFAULT_ALIGNABLE_ELEMENT_FILTER,
   DefaultTypes,
+  FeatureModule,
   GCompartment,
   GEdge,
   GGraph,
   GLSPProjectionView,
   GLabel,
   GLabelView,
+  IDiagramOptions,
   IHelperLineOptions,
   LogLevel,
   PolylineEdgeViewWithGapsOnIntersections,
   STANDALONE_MODULE_CONFIG,
   TYPES,
+  ViewerOptions,
   accessibilityModule,
   bindOrRebind,
   configureDefaultModelElements,
-  debugModule,
+  createDiagramOptionsModule,
   edgeEditToolModule,
   exportModule,
   gridModule,
   helperLineModule,
   initializeDiagramContainer,
-  keyboardToolPaletteModule,
   overrideModelElement,
   saveModule,
   toolPaletteModule
 } from '@eclipse-glsp/client';
 
 import { Inspector } from '../features/inspector';
+import { SaveModelKeyboardListener } from '../features/save-model';
 import { SvgExporter } from '../features/svg-exporter';
+import { ExternalServices } from './dynamic-external-services';
+import { StartupConfiguration } from './dynamic-startup-configurations';
 import { Container, ContainerModule } from 'inversify';
 
-import { StartupConfiguration } from './dynamic-startup-configurations';
+export const dynamicDiagramModule = (diagramOptions: IDiagramOptions) =>
+  new ContainerModule((bind, unbind, isBound, rebind) => {
+    const context = { bind, unbind, isBound, rebind };
 
-import { SaveModelKeyboardListener } from '../features/save-model';
+    // configure utilities, actions and services that needs to be loaded previously to the model
+    bind(StartupConfiguration).toSelf().inSingletonScope();
+    bind(TYPES.IDiagramStartup).toService(StartupConfiguration);
 
-export const dynamicDiagramModule = new ContainerModule((bind, unbind, isBound, rebind) => {
-  const context = { bind, unbind, isBound, rebind };
+    // configure the custom svg exporter to allow get svg from the GModel
+    bind(TYPES.SvgExporter).to(SvgExporter).inSingletonScope();
 
-  // configure utilities, actions and services that needs to be loaded previously to the model
-  bind(StartupConfiguration).toSelf().inSingletonScope();
-  bind(TYPES.IDiagramStartup).toService(StartupConfiguration);
+    // configure the save model action with (Ctrl + S) shortcut
+    bind(SaveModelKeyboardListener).toSelf().inSingletonScope();
+    bind(TYPES.KeyListener).toService(SaveModelKeyboardListener);
 
-  // configure the custom svg exporter to allow get svg from the GModel
-  bind(TYPES.SvgExporter).to(SvgExporter).inSingletonScope();
+    // configure the inspector that allow to edit elements abstract properties
+    bind(Inspector).toSelf().inSingletonScope();
+    bind(TYPES.IUIExtension).toService(Inspector);
+    bind(TYPES.IDiagramStartup).toService(Inspector);
+    bind(TYPES.ISelectionListener).toService(Inspector);
 
-  // configure the save model action with (Ctrl + S) shortcut
-  bind(SaveModelKeyboardListener).toSelf().inSingletonScope();
-  bind(TYPES.KeyListener).toService(SaveModelKeyboardListener);
+    // configure the default logger to show only warnings and errors
+    bindOrRebind(context, TYPES.LogLevel).toConstantValue(LogLevel.warn);
+    bindOrRebind(context, TYPES.ILogger).to(ConsoleLogger).inSingletonScope();
 
-  // configure the inspector that allow to edit elements abstract properties
-  bind(Inspector).toSelf().inSingletonScope();
-  bind(TYPES.IUIExtension).toService(Inspector);
-  bind(TYPES.IDiagramStartup).toService(Inspector);
-  bind(TYPES.ISelectionListener).toService(Inspector);
+    // configure the marquee tool behavior to select only the entire edge or element
+    bindOrRebind(context, TYPES.IMarqueeBehavior).toConstantValue({ entireEdge: true, entireElement: true });
 
-  // configure the default logger to show only warnings and errors
-  bindOrRebind(context, TYPES.LogLevel).toConstantValue(LogLevel.warn);
-  bindOrRebind(context, TYPES.ILogger).to(ConsoleLogger).inSingletonScope();
+    // configure the helper line options dynamically
+    bind<IHelperLineOptions>(TYPES.IHelperLineOptions).toDynamicValue(() => {
+      const options: IHelperLineOptions = {};
+      // skip compartments for alignment which are only used for structure
+      options.alignmentElementFilter = (element) =>
+        DEFAULT_ALIGNABLE_ELEMENT_FILTER(element) && !(element instanceof GCompartment);
+      return options;
+    });
 
-  // configure the marquee tool behavior to select only the entire edge or element
-  bindOrRebind(context, TYPES.IMarqueeBehavior).toConstantValue({ entireEdge: true, entireElement: true });
+    configureDefaultModelElements(context);
 
-  // configure the helper line options dynamically
-  bind<IHelperLineOptions>(TYPES.IHelperLineOptions).toDynamicValue(() => {
-    const options: IHelperLineOptions = {};
-    // skip compartments for alignment which are only used for structure
-    options.alignmentElementFilter = (element) =>
-      DEFAULT_ALIGNABLE_ELEMENT_FILTER(element) && !(element instanceof GCompartment);
-    return options;
+    // configure the GGraph (graphical model root element)
+    // if editMode is editable: render the projection view (scrollbars for spanning the diagram)
+    if (diagramOptions.editMode === 'editable')
+      overrideModelElement(context, DefaultTypes.GRAPH, GGraph, GLSPProjectionView);
+
+    // configure the GLabel (graphical model label element) with the GLabelView (default view for rendering labels)
+    // and enable the edit label feature (double click on label to edit)
+    overrideModelElement(context, DefaultTypes.LABEL, GLabel, GLabelView /*, { enable: [editLabelFeature] }*/); // disable edit label feature for now
+
+    // configure the GEdge (graphical model edge element) with the PolylineEdgeViewWithGapsOnIntersections view to render edges with gaps on intersections
+    overrideModelElement(context, DefaultTypes.EDGE, GEdge, PolylineEdgeViewWithGapsOnIntersections);
   });
 
-  configureDefaultModelElements(context);
+export function initializeDynamicDiagramContainer(
+  diagramOptions: IDiagramOptions,
+  viewerOptions: Partial<ViewerOptions>,
+  externalServices: ExternalServices,
+  ...extraModules: ContainerConfiguration
+): Container {
+  // create a new feature module for the external services (it can provide access to services outside GLSP)
+  const externalServicesModule = new FeatureModule(
+    (bind) => {
+      bind(ExternalServices).toConstantValue(externalServices);
+    },
+    { featureId: Symbol('externalServices') }
+  );
 
-  // configure the GGraph (graphical model root element) for render the projection view (scrollbars for spanning the diagram)
-  overrideModelElement(context, DefaultTypes.GRAPH, GGraph, GLSPProjectionView);
-
-  // configure the GLabel (graphical model label element) with the GLabelView (default view for rendering labels)
-  // and enable the edit label feature (double click on label to edit)
-  overrideModelElement(context, DefaultTypes.LABEL, GLabel, GLabelView /*, { enable: [editLabelFeature] }*/); // disable edit label feature for now
-
-  // configure the GEdge (graphical model edge element) with the PolylineEdgeViewWithGapsOnIntersections view to render edges with gaps on intersections
-  overrideModelElement(context, DefaultTypes.EDGE, GEdge, PolylineEdgeViewWithGapsOnIntersections);
-});
-
-export function initializeDynamicDiagramContainer(...containerConfiguration: ContainerConfiguration): Container {
   // create a new container and load all the modules that are needed
   // the modules are a set of bindings and configurations already defined in the GLSP client that can be reused or extended
   // the dynamicDiagramModule contains base custom bindings for our dynamic diagram language
   return initializeDiagramContainer(
     new Container(),
-    ...containerConfiguration,
+    createDiagramOptionsModule(diagramOptions, viewerOptions),
     STANDALONE_MODULE_CONFIG, // contains a set of default modules for basic features like undo/redo, copy/paste, selection, etc.
-    dynamicDiagramModule, // load all the bindings for the dynamic diagram language
-    helperLineModule, // load bindings for helper lines for alignment
-    gridModule, // load bindings for grid on the diagram and button to toggle the grid
-    edgeEditToolModule, // load bindings for debug mode (it can be enable with an option at command palette)
-    debugModule,
+    externalServicesModule, // load bindings for external services
     {
-      // add: accessibilityModule, // throws error when using editMode: READONLY (it need to be added on containerConfiguration)
+      add:
+        diagramOptions.editMode === 'editable'
+          ? [
+              accessibilityModule, // load bindings for accessibility features
+              gridModule, // load bindings for grid on the diagram and button to toggle the grid
+              helperLineModule, // load bindings for helper lines for alignment
+              edgeEditToolModule // load bindings for edge editing tool
+            ]
+          : [],
       remove: [toolPaletteModule, saveModule, exportModule]
-    }
+    },
+    dynamicDiagramModule(diagramOptions), // load all the bindings for the dynamic diagram language
+    ...extraModules
   );
 }
